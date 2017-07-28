e13064ba74a22ce4105909f94b5ebae74f54c0b7
ttata <tataterry89@gmail.com>
2017-07-27 21:44:10 -0700

    add skeleton endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,11 +10,23 @@ router.get('/', function(req, res, next) {
   // And insert something like this instead:
   res.json([{
   	id: 1,
-  	username: "samsepi0l"
+  	username: "[DEVELOPER NAME 1]"
   }, {
   	id: 2,
-  	username: "D0loresH4ze"
+  	username: "[DEVELOPER NAME 2]"
   }]);
 });
+
+router.post('/users', function(req, res) { });
+router.get('/users/:id', function(req, res) { });
+
+router.post('/benefits', function(req, res) { });
+router.get('/benefits/:id', function(req, res) { });
+router.delete('/benefits', function(req, res) { });
+
+router.post('/claims', function(req, res) { });
+router.get('/claims', function(req, res) { });
+router.get('/claims/:id', function(req, res) { });
+router.patch('/claims/:id', function(req, res) { });
 
 module.exports = router;
