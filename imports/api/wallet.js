475393ec04892f37c6c3176a0100ce8a32d69df4
Anshul Basia <anshul.dbgt@gmail.com>
2017-10-01 19:29:03 -0400

    Add-receive functionality completel

--- imports/api/wallet.js
+++ imports/api/wallet.js
@@ -16,7 +16,10 @@ if (Meteor.isServer) {
 Meteor.methods({
     'wallet.insert'(address,bal) {
        
-        console.log("damn");
+        var userId = Meteor.userId();
+        var user = Meteor.users.findOne({_id: userId});
+        var email = user.emails[0].address;
+        
         // Ensure user logged in
         if (!this.userId)
             throw new Meteor.Error('not-authorized');
@@ -26,57 +29,44 @@ Meteor.methods({
         Wallet.insert({
             wallet: address,
             balance: bal,
+            email: email,
             owner: this.userId,
             createdAt: new Date(),
         });
     },
-    // 'wallet.deactivate'(policyId) {
-    //     check(policyId, String);
 
-    //     // Ensure user owns policy
-    //     const policy = Policies.findOne(policyId);
-    //     if (policy.owner !== this.userId)
-    //         throw new Meteor.Error('not-authorized');
+    'wallet.update'(id,amount,wallet,email) {
+       
+        if(bal<0)
+            throw new Meteor.Error('Insufficient Balance');
+        // Ensure user logged in
+        if (!this.userId)
+            throw new Meteor.Error('not-authorized');
+        
+        var current = Wallet.findOne({_id: id}).balance;
+      
+        var balance = current-amount;
+        // Update Wallet of user
+        Wallet.update(id, { $set: {
+            wallet,
+            balance,
+        } });
 
-    //     // TODO charge UMC fee
-    //     // TODO return remaining UMC
+        console.log(id+balance+wallet);
+        //Update Wallet of recepient
+        var user = Wallet.findOne({email: email});
+        var recepient_id = user._id;
+        var recepient_address = user.wallet;
+        var recepient_bal = user.balance;
+        
 
-    //     // TODO consider moving this to claims.js
-    //     // Deactivate outstanding claims
-    //     Claims.update({ owner: this.userId, active: true }, { $set: {
-    //         active: false,
-    //         deactivatedAt: new Date(),
-    //     }}, {multi: true});
+        id = recepient_id;
+        wallet = recepient_address;
+        balance = recepient_bal + amount;
+        Wallet.update(id, { $set: {
+            wallet,
+            balance,
+        } });
 
-    //     // Update policy
-    //     Policies.update(policyId, { $set: {
-    //         active: false,
-    //         deactivatedAt: new Date(),
-    //     }});
-    // },
-    // 'wallet.addFunds'(policyId, amountAdditional) {
-    //     check(policyId, String);
-    //     check(amountAdditional, Match.Integer);
-
-    //     // Ensure user owns active policy
-    //     const policy = Policies.findOne(policyId);
-    //     if (policy.owner !== this.userId && policy.active)
-    //         throw new Meteor.Error('not-authorized');
-
-    //     // TODO take UMC
-
-    //     // Calculate policy data
-    //     const amount = policy.amount + amountAdditional;
-    //     const amountInitial =  policy.amountInitial + amountAdditional;
-    //     const payoutMax = amountInitial * PAYOUT_MULTIPLIER;
-    //     const payoutRemaining = policy.payoutRemaining + amountAdditional * PAYOUT_MULTIPLIER;
-
-    //     // Update policy
-    //     Policies.update(policyId, { $set: {
-    //         amount,
-    //         amountInitial,
-    //         payoutMax,
-    //         payoutRemaining,
-    //     } });
-    // },
+    },
 });
--- imports/ui/pages/UmcWallet.jsx
+++ imports/ui/pages/UmcWallet.jsx
@@ -1,105 +1,151 @@
-import React, { Component, PropTypes } from 'react';
-import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
+    import React, { Component, PropTypes } from 'react';
+    import { Meteor } from 'meteor/meteor';
+    import { createContainer } from 'meteor/react-meteor-data';
+    import classnames from 'classnames';
 
-import { Claims } from '../../api/claims.js';
-import { Policies } from '../../api/policies.js';
-import { Wallet } from '../../api/wallet.js';
-import ClaimCard from '../components/ClaimCard.jsx';
-import ClaimListCard from '../components/ClaimListCard.jsx';
-import PolicyCard from '../components/PolicyCard.jsx';
+    import {categoryToIcon} from "../../helpers/categoryHelper"
+    import { Claims } from '../../api/claims.js';
+    import { Policies } from '../../api/policies.js';
+    import { Wallet } from '../../api/wallet.js';
+    import ClaimCard from '../components/ClaimCard.jsx';
+    import ClaimListCard from '../components/ClaimListCard.jsx';
+    import PolicyCard from '../components/PolicyCard.jsx';
 
 
-class UmcWallet extends Component {
-    
-    address(){
-        return this.props.wallet;
-    }
 
-    handleFileClick(event) {
-        event.preventDefault();
-        var address = Math.floor(Math.random()*900000) + 100000;
-        var balance = 0;
-        Meteor.call('wallet.insert', address, balance);
-    }
+    class UmcWallet extends Component {
+        
+        address(){
+            return this.props.wallet;
+        }
 
-    renderLoggedInWithWallet() {
-        let res = this.address();
-        return (
-            <div className="section">
-                <h3> Wallet</h3>
-                <div className="row">
-                    <div className="col s12">
-                       UMC Wallet Address: {res[0].wallet}
-                    </div>
-                    <div className="col s12">
-                       UMC Wallet Balance: {res[0].balance}
-                    </div>
-                    <div className="col s12">
-                       Add/Withdraw Balance
-                    </div>
-                    <div className="col s12">
-                       Send UMC coins
-                    </div>
-                    <div className="col s12">
-                       Request UMC coins
+        handleFileClick(event) {
+            event.preventDefault();
+            var address = Math.floor(Math.random()*900000) + 100000;
+            var balance = 0;
+            Meteor.call('wallet.insert', address, balance);
+        }
+
+        handleFileClickforSend(event) {
+            console.log("f");
+            event.preventDefault();
+            var email =  this.refs.email.value.trim();
+            var amount =  parseInt(this.refs.amount.value.trim());
+            
+            let res = this.address();
+            id = res[0]._id;
+            address = res[0].wallet;
+           
+           
+            Meteor.call('wallet.update',id,amount,address,email);
+        }
+
+        renderLoggedInWithWallet() {
+            let res = this.address();
+            return (
+                <div className="section">
+                    <h3> Wallet</h3>
+                    <div className="row">
+                        <div className="col s12">
+                        UMC Wallet Address: {res[0].wallet}
+                        </div>
+                        <br/>
+                        <div className="col s12">
+                        UMC Wallet Balance: {res[0].balance}
+                        </div>
+                        <br/>
+                        <div className="col s12">
+                        Add/Withdraw Balance
+                        </div>
+                        <br/>
+                        <div className="col s2">
+                        <h5>Send UMC coins</h5>
+                        <form onSubmit={this.handleFileClickforSend.bind(this)}>
+                            <div>
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email" ref="email" placeholder="Receiver's Email Address"/>
+                            </div>
+                            <div>
+                                <label htmlFor="amount">Amount</label>
+                                <input type="number" ref="amount" placeholder="Amount"/>
+                            </div>
+                            <div>
+                                <button className="btn-large waves-effect waves-light" type="submit">Send</button>
+                            </div>
+                            <br/>
+                        </form>
+                        </div>
+                        <br/>
+                        <div className="col s4">
+                        <h5>Request UMC coins</h5>
+                        <form onSubmit={this.handleFileClickforSend.bind(this)}>
+                            <div>
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email" ref="email" placeholder="Receiver's Email Address"/>
+                            </div>
+                            <div>
+                                <button className="btn-large waves-effect waves-light" type="submit">Request</button>
+                            </div>
+                            <br/>
+                        </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
-    }
+            );
+        }
 
-    renderLoggedInWithoutWallet() {
-       
-        return (
-            <div className="section">
-                <div className="col s4">
-                Add Wallet
-                <a href="#" onClick={this.handleFileClick.bind(this)}>
-                <i className="material-icons">add</i>
-                </a>
+        renderLoggedInWithoutWallet() {
+        
+            return (
+                <div className="section">
+                    <div className="col s4">
+                    Add Wallet
+                    <a href="#" onClick={this.handleFileClick.bind(this)}>
+                    <i className="material-icons">add</i>
+                    </a>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
+
+        renderLoggedIn(){
+            console.log(this.props.wallet);
+            return this.props.wallet.length>0 ? this.renderLoggedInWithWallet() : this.renderLoggedInWithoutWallet();
+        }
+
+        renderLoggedOut() {
+            return (
+                <div className="section">
+                    <h4>Please log in </h4>
+                    
+                </div>
+            );
+        }
+
+        render() {
+            console.log(this.props.curentUser);
+            return this.props.currentUser ? this.renderLoggedIn() : this.renderLoggedOut();
+        }
     }
 
-    renderLoggedIn(){
-        console.log(this.props.wallet);
-        return this.props.wallet.length>0 ? this.renderLoggedInWithWallet() : this.renderLoggedInWithoutWallet();
-    }
-
-    renderLoggedOut() {
-        return (
-            <div className="section">
-                <h4>Please log in </h4>
-                
-            </div>
-        );
-    }
-
-    render() {
-        return this.props.currentUser ? this.renderLoggedIn() : this.renderLoggedOut();
-    }
-}
-
-UmcWallet.propTypes = {
-    claims: PropTypes.array.isRequired,
-    communityClaims: PropTypes.array.isRequired,
-    currentUser: PropTypes.object,
-    policy: PropTypes.object,
-    wallet: React.PropTypes.object,
-};
-
-export default createContainer(() => {
-    
-    Meteor.subscribe('claims');
-    Meteor.subscribe('policies');
-    Meteor.subscribe('wallet');
-    return {
-        communityClaims: Claims.find({ owner: { $ne: Meteor.userId() } }, { sort: { createdAt: -1 } }).fetch(),
-        claims: Claims.find({ owner: Meteor.userId() }, { sort: { createdAt: -1 } }).fetch(),
-        currentUser: Meteor.user(),
-        policy: Policies.findOne({ owner: Meteor.userId() }),
-        wallet: Wallet.find({ owner: Meteor.userId() }, { sort: { createdAt: -1 } }).fetch(),
+    UmcWallet.propTypes = {
+        claims: PropTypes.array.isRequired,
+        communityClaims: PropTypes.array.isRequired,
+        currentUser: PropTypes.object,
+        policy: PropTypes.object,
+        wallet: React.PropTypes.object,
     };
-}, UmcWallet);
+
+    export default createContainer(() => {
+        
+        Meteor.subscribe('claims');
+        Meteor.subscribe('policies');
+        Meteor.subscribe('wallet');
+        return {
+            communityClaims: Claims.find({ owner: { $ne: Meteor.userId() } }, { sort: { createdAt: -1 } }).fetch(),
+            claims: Claims.find({ owner: Meteor.userId() }, { sort: { createdAt: -1 } }).fetch(),
+            currentUser: Meteor.user(),
+            policy: Policies.findOne({ owner: Meteor.userId() }),
+            wallet: Wallet.find({ owner: Meteor.userId() }, { sort: { createdAt: -1 } }).fetch(),
+        };
+    }, UmcWallet);
