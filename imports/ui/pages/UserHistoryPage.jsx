9ee14ce7ebb82bb870b36dcba0cc13d1aa4b096e
Sarisht Wadhwa <sarishtwadhwa@Kumuds-MacBook-Air.local>
2017-12-18 10:02:26 +0530

    changes

--- imports/ui/pages/UserHistoryPage.jsx
+++ imports/ui/pages/UserHistoryPage.jsx
@@ -1,32 +1,9 @@
-import { createContainer } from 'meteor/react-meteor-data';
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
 
-import { Claims } from '../../api/claims.js';
-import ClaimListCard from '../components/ClaimListCard.jsx';
-
-class UserHistoryPage extends React.Component {
+export default class UserHistoryPage extends React.Component {
     render() {
         return (
-           	<div className="section">
-                <div className="row">
-                    <div className="col s12 l4">
-                        <ClaimListCard claims={this.props.claims}/>
-                    </div>
-                </div>
-            </div>
+            <div>User History Page</div>
         );
     }
 }
-
-UserHistoryPage.propTypes = {
-    claims: React.PropTypes.array.isRequired,
-};
-
-export default createContainer(() => {
-    Meteor.subscribe('claims');
-
-    return {
-        claims: Claims.find({ owner: Meteor.userId() }, { sort: { createdAt: -1 } }).fetch(),
-    };
-}, UserHistoryPage);
